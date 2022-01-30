919ae80240dfd8f5bece99c96af9d295d8ada942
Sergey Romanov <sergey.romanov@gmail.com>
2022-01-30 03:47:40 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,15 @@
 
 set -e
 
-sh -c "vsce $*"
+sh -c """
+if [$1]
+then
+    sh -c "vsce package"
+    filelist=(`ls ./*.vsix'`)
+    file=`realpath "$PWD/$filelist[*]"`
+    echo "::notice ::Generating a VSIX file at $file"
+    echo "::set-output name=vsix_path::$file"
+fi
+
+vsce publish -p $2
+"""
