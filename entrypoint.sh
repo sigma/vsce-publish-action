cbb4eb60ab2d9003c0ea649b50de02f715f93181
serhioromano <serhioromano@outlook.com>
2022-01-30 21:17:22 +0600

    test fa

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/sh -l
 
 set -e
 
-sh -c """
-if [$1]
-then
-    sh -c "vsce package"
-    filelist=(`ls ./*.vsix'`)
-    file=`realpath "$PWD/$filelist[*]"`
-    echo "::notice ::Generating a VSIX file at $file"
-    echo "::set-output name=vsix_path::$file"
+if [ $1 == "yes" ]; then
+    vsce package
+    filelist=$(ls *.vsix)
+    file=$(realpath "${PWD}a/${filelist}")
+    fa=$(echo "${file}" | grep "No such file")
+    echo "::notice ::What we found ${fa}"
+    echo "::notice ::Generating a VSIX file at ${file}"
+    echo "::set-output name=vsix_path::${file}"
 fi
-
 vsce publish -p $2
-"""
+
+
